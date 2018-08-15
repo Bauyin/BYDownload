c76fefab64113f4f71ba9b1fc795f1d12662b6ba
Cuibaoyin <cuibaoyin@icloud.com>
2018-08-15 10:41:01 +0800

    Initial Commit

---
+++ BYDownload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BYDownload
+//
+//  Created by 崔宝印 on 2018/8/15.
+//  Copyright © 2018年 Koolearn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BYDownload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BYDownload
+//
+//  Created by 崔宝印 on 2018/8/15.
+//  Copyright © 2018年 Koolearn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
